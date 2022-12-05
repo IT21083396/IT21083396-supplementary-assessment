c03ed5587f4672e2a8e0222d152ab71070432760
galaa <it21083396@my.sliit.lk>
2022-12-05 23:03:23 +0530

    create UI/UX for user login

---
+++ app/src/main/java/com/example/it21083396supplementaryassessment/profileManagement.java
@@ -0,0 +1,14 @@
+package com.example.it21083396supplementaryassessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profileManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_management);
+    }
+}
